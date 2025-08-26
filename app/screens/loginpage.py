afbe06ce08a6e1d2ef7505dd6be1ce5509be01a7
SanjanaV5103 <sanjanav5103@gmail.com>
2025-08-26 14:10:08 +0530

    Ride requestt

--- app/screens/loginpage.py
+++ app/screens/loginpage.py
@@ -44,15 +44,9 @@ class LoginPage(ctk.CTkFrame):
                 self.status_label.configure(text="Login Successful!", text_color="green")
 
                 if self.manager: 
-<<<<<<< HEAD
                     if hasattr(Config.loggedInUser, 'role') and Config.loggedInUser['role'] == 'rider':
                         self.manager.show_screen("ride_request")
                     elif hasattr(Config.loggedInUser, 'role') and Config.loggedInUser['role'] == 'driver':
-=======
-                    if Config.loggedInUser['role'] == 'rider':
-                        self.manager.show_screen("rider_dashboard")
-                    elif Config.loggedInUser['role'] == 'driver':
->>>>>>> d9b8a41e89bff0a8a33960f372ef96d0687e6e5b
                         self.manager.show_screen("driver_dashboard")
                     # elif Config.loggedInUser['role'] == 'admin':
                     #     self.manager.show_screen("rider_dashboard")
